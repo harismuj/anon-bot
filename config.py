788b4314bed18a0f241902faf0c91c7d273d57b6
Haris Mujianto <139291554+harismuj@users.noreply.github.com>
2024-08-18 01:08:14 +0700

    new

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-TOKEN = "6036728228:AAHmnhauraZJ_t4BewRrjUGGTjIuQfJ0-7I"
-#1931787787:AAERMWZ10rGe4DIjxUnUpsDOwM8c-vIhLtw
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
